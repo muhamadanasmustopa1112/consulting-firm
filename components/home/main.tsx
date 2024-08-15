1b21366c651cf5dbb9e1968695fa8181b048ae46
Fajar Muhamad <alafajar@gmail.com>
2024-08-15 22:25:11 +0700

    added mobile view (cta, main, and solution)

--- components/home/main.tsx
+++ components/home/main.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function MainHome() {
-    return (
-        <main className="min-h-[80vh] flex flex-col justify-center items-start px-32 bg-[url('/img/bg-home.png')] bg-no-repeat bg-cover bg-right-bottom relative z-10 before:content-[''] before:absolute before:w-full before:h-full before:block before:bg-gradient-to-b before:from-transparent before:from-60% before:to-black before:to-95% before:-z-10 before:top-0 before:left-0">
-            <div className="flex flex-col gap-2">
-                <h1 className="text-4xl font-semibold">
-                    IURIS: Of One&apos;s Own Right,<br />
-                    A Universe of Opportunities
-                </h1>
-                <h2 className="text-base">
-                    Unlock Growth Potential with Human-AI Synergy, Comprehensive<br />Consultation, and Profit Opportunities for Corporate Excellence.
-                </h2>
-            </div>
-        </main>
-    )
+  return (
+    <main className="min-h-[calc(100dvh_-_74px)] pt-10 md:pt-0 md:min-h-[80vh] flex flex-col justify-start md:justify-center items-start px-4 md:px-32 bg-[url('/img/bg-home.png')] bg-no-repeat bg-cover bg-right-bottom relative z-10 before:content-[''] before:absolute before:w-full before:h-full before:block before:bg-gradient-to-b before:from-transparent before:from-60% before:to-black before:to-95% before:-z-10 before:top-0 before:left-0">
+      <div className="flex flex-col gap-2">
+        <h1 className="text-4xl font-bold leading-[40px]">
+          IURIS: Of One&apos;s Own Right,
+          <br />A Universe of Opportunities
+        </h1>
+        <h2 className="mt-4 text-base leading-6">
+          Unlock Growth Potential with Human-AI Synergy, Comprehensive
+          <br />
+          Consultation, and Profit Opportunities for Corporate Excellence.
+        </h2>
+      </div>
+    </main>
+  );
 }
 
-export default MainHome
+export default MainHome;
--- components/home/solutions.tsx
+++ components/home/solutions.tsx
@@ -1,34 +1,38 @@
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function Solutions() {
-    const solutions = [
-        "Consulting Services",
-        "Marketing Solutions",
-        "Staffing Support",
-        "Auxiliary Services",
-        "Global Business Transitions",
-        "AI Consulting Services",
-    ];
-    return (
-        <section className='p-32 pt-0 flex flex-col gap-14 relative z-20'>
-            <div className="flex flex-col gap-2 text-center">
-                <h3 className='text-4xl font-semibold'>Our Solutions</h3>
-                <h4>Tailored AI Bento Box Business Package for Our Clients</h4>
-            </div>
-            <div className="flex flex-row flex-wrap justify-center items-center gap-6">
-
-                {
-                    solutions.map((item, idx) =>
-                    (
-                        <Link href="" key={idx} className="border border-white rounded-tl-[1.875rem] rounded-br-[1.875rem] p-4 transition-colors hover:bg-grey">
-                            <p className='text-2xl font-semibold'>{item}</p>
-                        </Link>)
-                    )
-                }
-            </div>
-        </section>
-    )
+  const solutions = [
+    "Consulting Services",
+    "Marketing Solutions",
+    "Staffing Support",
+    "Auxiliary Services",
+    "Global Business Transitions",
+    "AI Consulting Services",
+  ];
+  return (
+    <section className="pt-10 px-4 md:p-32 md:pt-0 flex flex-col gap-8 md:gap-14 relative z-20">
+      <div className="flex flex-col gap-2 text-center">
+        <h3 className="text-4xl font-semibold leading-10">Our Solutions</h3>
+        <h4 className="mx-auto w-[308px] md:w-full">
+          Tailored AI Bento Box Business Package for Our Clients
+        </h4>
+      </div>
+      <div className="flex flex-row flex-wrap justify-center items-center gap-6">
+        {solutions.map((item, idx) => (
+          <Link
+            href=""
+            key={idx}
+            className="border border-white rounded-tl-[1.875rem] rounded-br-[1.875rem] p-4 transition-colors hover:bg-grey w-[300px] md:w-auto"
+          >
+            <p className="text-lg md:text-2xl font-semibold text-center">
+              {item}
+            </p>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Solutions
+export default Solutions;
