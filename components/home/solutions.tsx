789187e5b5dc2ba1b43f806c11a0fc3fb9a0ac7a
Muhammad Kholil Aziz Ashari <emkahaaa@gmail.com>
2024-08-15 09:03:27 +0700

    fix: add link on our solution

--- components/home/solutions.tsx
+++ components/home/solutions.tsx
@@ -1,34 +1,52 @@
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function Solutions() {
-    const solutions = [
-        "Consulting Services",
-        "Marketing Solutions",
-        "Staffing Support",
-        "Auxiliary Services",
-        "Global Business Transitions",
-        "AI Consulting Services",
-    ];
-    return (
-        <section className='p-32 pt-0 flex flex-col gap-14 relative z-20'>
-            <div className="flex flex-col gap-2 text-center">
-                <h3 className='text-4xl font-semibold'>Our Solutions</h3>
-                <h4>Tailored AI Bento Box Business Package for Our Clients</h4>
-            </div>
-            <div className="flex flex-row flex-wrap justify-center items-center gap-6">
-
-                {
-                    solutions.map((item, idx) =>
-                    (
-                        <Link href="" key={idx} className="border border-white rounded-tl-[1.875rem] rounded-br-[1.875rem] p-4 transition-colors hover:bg-grey">
-                            <p className='text-2xl font-semibold'>{item}</p>
-                        </Link>)
-                    )
-                }
-            </div>
-        </section>
-    )
+  const solutions = [
+    {
+      name: "Consulting Services",
+      link: "services/consulting",
+    },
+    {
+      name: "Marketing Solutions",
+      link: "services/marketing",
+    },
+    {
+      name: "Staffing Support",
+      link: "services/staffing",
+    },
+    {
+      name: "Auxiliary Services",
+      link: "services/auxiliary-services",
+    },
+    {
+      name: "Global Business Transitions",
+      link: "services/business-transition",
+    },
+    {
+      name: "AI Consulting Services",
+      link: "services/ai-services",
+    },
+  ];
+  return (
+    <section className="p-32 pt-0 flex flex-col gap-14 relative z-20">
+      <div className="flex flex-col gap-2 text-center">
+        <h3 className="text-4xl font-semibold">Our Solutions</h3>
+        <h4>Tailored AI Bento Box Business Package for Our Clients</h4>
+      </div>
+      <div className="flex flex-row flex-wrap justify-center items-center gap-6">
+        {solutions.map((item, idx) => (
+          <Link
+            href={item.link}
+            key={idx}
+            className="border border-white rounded-tl-[1.875rem] rounded-br-[1.875rem] p-4 transition-colors hover:bg-grey"
+          >
+            <p className="text-2xl font-semibold">{item.name}</p>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Solutions
+export default Solutions;
