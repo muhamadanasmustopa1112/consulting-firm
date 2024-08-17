db5915c19df0a937828857c3bee9e5399d45917e
Muhammad Kholil Aziz Ashari <emkahaaa@gmail.com>
2024-08-17 15:23:51 +0700

    fix: mobile & ipad responsive coworking

--- components/coworking/about.tsx
+++ components/coworking/about.tsx
@@ -1,24 +1,46 @@
-import Image from 'next/image'
-import React from 'react'
-import sourceSansPro from '@/components/ui/sourceSansPro'
+import Image from "next/image";
+import React from "react";
+import sourceSansPro from "@/components/ui/sourceSansPro";
 
 const AboutCoworking = () => {
-    return (
-        <div className='border border-white rounded-tl-[1.875rem] rounded-br-[1.875rem] p-4 flex flex-row gap-10 items-center relative z-10'>
-            <div className='max-sm:hidden absolute -z-10 h-[150%] w-auto aspect-square -bottom-1/2 left-0 before:relative before:-z-10 before:content-[""] before:block before:bg-white before:h-[90%] before:w-[90%] before:rounded-full before:blur-3xl before:bg-opacity-10 before:-left-2/3 before:top-1/2 before:-translate-y-1/2'></div>
-            <div className='flex-1 rounded-tl-[1rem] overflow-hidden'>
-                <Image src="/img/coworking/about.png" alt='' width={400} height={400} className='object-cover w-full h-full' />
-            </div>
-            <div className='flex-1'>
-                <h2 className='text-xl font-semibold mb-2'>About Our Business Space</h2>
-                <p className={sourceSansPro.className}>
-                    IURIS Coworking space are located in the most prestigious part of the city and offers a variety of workspace options including private offices, dedicated desks, and hot desks. Our space is designed with prestige in mind and is equipped with premium facilities to help you achieve your goals.
-                    <br /><br />
-                    IURIS is designed to provide a prestigious and productive work environment for high-end professionals and entrepreneurs. With exceptional amenities and services, our space is the perfect solution for those seeking a prestigious workspace experience prestigious workspace experience in Autograph Tower Thamrin Nine level. 35th, Central Jakarta.
-                </p>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="border border-white rounded-tl-[1.875rem] rounded-br-[1.875rem] p-4 md:p-6 lg:p-8 flex flex-col md:flex-row gap-4 md:gap-10 items-center relative z-10">
+      <div className='hidden md:block absolute -z-10 h-[150%] w-auto aspect-square -bottom-1/2 left-0 before:relative before:-z-10 before:content-[""] before:block before:bg-white before:h-[90%] before:w-[90%] before:rounded-full before:blur-3xl before:bg-opacity-10 before:-left-2/3 before:top-1/2 before:-translate-y-1/2'></div>
+      <div className="w-full md:w-1/2 lg:w-1/3 rounded-tl-[1rem] overflow-hidden">
+        <Image
+          src="/img/coworking/about.png"
+          alt=""
+          width={400}
+          height={400}
+          className="object-cover w-full h-full"
+        />
+      </div>
+      <div className="w-full md:w-1/2 lg:w-2/3">
+        <h2 className="text-lg md:text-xl lg:text-2xl font-semibold mb-2">
+          About Our Business Space
+        </h2>
+        <p
+          className={
+            sourceSansPro.className + " text-sm md:text-base lg:text-lg"
+          }
+        >
+          IURIS Coworking space are located in the most prestigious part of the
+          city and offers a variety of workspace options including private
+          offices, dedicated desks, and hot desks. Our space is designed with
+          prestige in mind and is equipped with premium facilities to help you
+          achieve your goals.
+          <br />
+          <br />
+          IURIS is designed to provide a prestigious and productive work
+          environment for high-end professionals and entrepreneurs. With
+          exceptional amenities and services, our space is the perfect solution
+          for those seeking a prestigious workspace experience prestigious
+          workspace experience in Autograph Tower Thamrin Nine level. 35th,
+          Central Jakarta.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default AboutCoworking
+export default AboutCoworking;
--- components/coworking/info.tsx
+++ components/coworking/info.tsx
@@ -1,48 +1,82 @@
-import React from 'react'
-import sourceSansPro from '@/components/ui/sourceSansPro'
+import React from "react";
+import sourceSansPro from "@/components/ui/sourceSansPro";
 
 const Info = () => {
-    return (
-        <div className='flex flex-row gap-8 relative z-10'>
-            <div className='max-sm:hidden absolute -z-10 h-[150%] w-auto aspect-square -bottom-3/4 right-0 before:relative before:-z-10 before:content-[""] before:block before:bg-white before:h-[90%] before:w-[90%] before:rounded-full before:blur-3xl before:bg-opacity-10 before:-right-2/3 before:top-1/2 before:-translate-y-1/4'></div>
-            <div className="flex-1 border border-white rounded-tl-[1.875rem] rounded-br-[1.875rem] p-4">
-                <h2 className='uppercase text-xl font-semibold text-center mb-4'>How IURIS Coworking is Different from others?</h2>
-                <ul className={`${sourceSansPro.className} leading-snug mx-6`}>
-                    <li className='list-disc'>Larger spacious offices</li>
-                    <li className='list-disc'>High end executive hub community</li>
-                    <li className='list-disc'>High level floors</li>
-                    <li className='list-disc'>Large ceilings</li>
-                    <li className='list-disc'>Reasonable pricing</li>
-                    <li className='list-disc'>Modern luxury aesthetic with customizeable options</li>
-                    <li className='list-disc'>High end prime locations</li>
-                    <li className='list-disc'>In office lounge/restaurant/bar/cigar room</li>
-                    <li className='list-disc'>Modular office spaces</li>
-                    <li className='list-disc'>Large virtual office membership solutions available</li>
-                </ul>
-            </div>
-            <div className="flex-1 border border-white rounded-tl-[1.875rem] rounded-br-[1.875rem] p-4">
-                <h2 className='uppercase text-xl font-semibold text-center mb-4'>Exclusive Community and Networking</h2>
-                <p className={sourceSansPro.className + " text-center"}>
-                    In addition to the workspace, we offer an exclusive community of high-end professionals and entrepreneurs. Our space hosts a variety of exclusive events, workshops, and networking opportunities to help you connect and collaborate with like-minded individuals in your industry
-                </p>
-            </div>
-            <div className="flex-1 border border-white rounded-tl-[1.875rem] rounded-br-[1.875rem] p-4">
-                <h2 className='uppercase text-xl font-semibold text-center mb-4'>Amenities</h2>
-                <p className={sourceSansPro.className}>
-                    At IURIS Coworking Space, we provide exceptional amenities and services to cater to your every need. These include:
-                </p>
-                <ul className={sourceSansPro.className + " leading-snug mx-8"}>
-                    <li className='list-disc'>High-speed internet</li>
-                    <li className='list-disc'>World-class meeting rooms</li>
-                    <li className='list-disc'>Printing and scanning facilities</li>
-                    <li className='list-disc'>Gourmet kitchen and lounge areas</li>
-                    <li className='list-disc'>Event hall</li>
-                    <li className='list-disc'>Personalized reception and mail services</li>
-                    <li className='list-disc'>24/7 access</li>
-                </ul>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col md:flex-row gap-8 relative z-10">
+      <div className='hidden md:block absolute -z-10 h-[150%] w-auto aspect-square -bottom-3/4 right-0 before:relative before:-z-10 before:content-[""] before:block before:bg-white before:h-[90%] before:w-[90%] before:rounded-full before:blur-3xl before:bg-opacity-10 before:-right-2/3 before:top-1/2 before:-translate-y-1/4'></div>
+      <div className="flex-1 border border-white rounded-tl-[1.875rem] rounded-br-[1.875rem] p-4 md:p-6 lg:p-8">
+        <h2 className="uppercase text-lg md:text-xl lg:text-2xl font-semibold text-center mb-4">
+          How IURIS Coworking is Different from Others?
+        </h2>
+        <ul
+          className={`${sourceSansPro.className} text-sm md:text-base lg:text-lg leading-snug mx-4 md:mx-6 lg:mx-8`}
+        >
+          <li className="list-disc">Larger spacious offices</li>
+          <li className="list-disc">High end executive hub community</li>
+          <li className="list-disc">High level floors</li>
+          <li className="list-disc">Large ceilings</li>
+          <li className="list-disc">Reasonable pricing</li>
+          <li className="list-disc">
+            Modern luxury aesthetic with customizable options
+          </li>
+          <li className="list-disc">High end prime locations</li>
+          <li className="list-disc">
+            In office lounge/restaurant/bar/cigar room
+          </li>
+          <li className="list-disc">Modular office spaces</li>
+          <li className="list-disc">
+            Large virtual office membership solutions available
+          </li>
+        </ul>
+      </div>
+      <div className="flex-1 border border-white rounded-tl-[1.875rem] rounded-br-[1.875rem] p-4 md:p-6 lg:p-8">
+        <h2 className="uppercase text-lg md:text-xl lg:text-2xl font-semibold text-center mb-4">
+          Exclusive Community and Networking
+        </h2>
+        <p
+          className={
+            sourceSansPro.className +
+            " text-sm md:text-base lg:text-lg text-center"
+          }
+        >
+          In addition to the workspace, we offer an exclusive community of
+          high-end professionals and entrepreneurs. Our space hosts a variety of
+          exclusive events, workshops, and networking opportunities to help you
+          connect and collaborate with like-minded individuals in your industry.
+        </p>
+      </div>
+      <div className="flex-1 border border-white rounded-tl-[1.875rem] rounded-br-[1.875rem] p-4 md:p-6 lg:p-8">
+        <h2 className="uppercase text-lg md:text-xl lg:text-2xl font-semibold text-center mb-4">
+          Amenities
+        </h2>
+        <p
+          className={
+            sourceSansPro.className + " text-sm md:text-base lg:text-lg"
+          }
+        >
+          At IURIS Coworking Space, we provide exceptional amenities and
+          services to cater to your every need. These include:
+        </p>
+        <ul
+          className={
+            sourceSansPro.className +
+            " text-sm md:text-base lg:text-lg leading-snug mx-4 md:mx-6 lg:mx-8"
+          }
+        >
+          <li className="list-disc">High-speed internet</li>
+          <li className="list-disc">World-class meeting rooms</li>
+          <li className="list-disc">Printing and scanning facilities</li>
+          <li className="list-disc">Gourmet kitchen and lounge areas</li>
+          <li className="list-disc">Event hall</li>
+          <li className="list-disc">
+            Personalized reception and mail services
+          </li>
+          <li className="list-disc">24/7 access</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Info
+export default Info;
